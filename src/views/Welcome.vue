82af0e243ec35a65fc5d9cb9c1726a64ddb9b1d1
coder-isiahjones <jones.isiah.blhs@gmail.com>
2023-04-02 13:30:02 -0400

    init

---
+++ src/views/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="welcome container">
+    <p>Welcome</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.welcome {
+  text-align: center;
+  padding: 1.25rem 0;
+}
+</style>
